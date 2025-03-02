ff205e033be5e8ed5301318191e76e1a5fcb7aae
gohj99 <62464085+gohj99@users.noreply.github.com>
2025-03-02 17:02:19 +0800

    Edit versionName
    Signed-off-by: gohj99 <62464085+gohj99@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 24
+        versionCode = 25
         versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
